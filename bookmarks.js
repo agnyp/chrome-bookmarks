54fec1326e29b64baf6a0f633509b6fa63c70fcd
Andreas Gnyp <andreas@gnyp.de>
2018-05-21 20:18:52 +0200

    Listing - very dirty - top-level folders
    Note: Not decided on using any frameworks yet.

--- bookmarks.js
+++ bookmarks.js
@@ -2,6 +2,10 @@
 
 chrome.runtime.onInstalled.addListener(function() {
   console.log("Plugin installed ... I suppose.");
+  chrome.bookmarks.getTree(function(bookmarks){
+    console.log(bookmarks)
+    console.log(bookmarks[0])
+  })
   chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
     chrome.declarativeContent.onPageChanged.addRules([{
       conditions: [new chrome.declarativeContent.PageStateMatcher({
--- popup.js
+++ popup.js
@@ -1,3 +1,17 @@
 chrome.bookmarks.getTree(function(bookmarks){
-  console.log(bookmarks)
+  const section = document.getElementById("bookmarks")
+  bookmarks[0].children.forEach((child) => {
+    const p = document.createElement('p')
+    p.innerHTML = child.title
+    section.appendChild(p)
+  })
 })
+
+// Tools
+const bm = {}
+
+bm.openBgTab = (link) => {
+  chrome.tabs.create({url:link,active:false},function(tab){
+    console.log("tab",tab)
+  })
+}
