b645c472650d9bb6b03127942300e9bc18f2a81a
Andreas Gnyp <andreas.gnyp@sinnerschrader.com>
2018-05-21 23:56:39 +0200

    half-recursive

--- popup.js
+++ popup.js
@@ -1,9 +1,13 @@
 chrome.bookmarks.getTree(function(bookmarks){
+  const bmCategories = bookmarks[0]
+
   const section = document.getElementById("bookmarks")
-  bookmarks[0].children.forEach((child) => {
-    const p = document.createElement('p')
-    p.innerHTML = child.title
-    section.appendChild(p)
+
+  bmCategories.children.forEach((category) => {
+    const h2 = document.createElement('h2')
+    h2.innerHTML = category.title
+    section.appendChild(h2)
+    bm.appendChild(h2, category)
   })
 })
 
@@ -14,4 +18,21 @@ bm.openBgTab = (link) => {
   chrome.tabs.create({url:link,active:false},function(tab){
     console.log("tab",tab)
   })
+}
+bm.appendChild = (elem, child) => {
+  if(child.children == null){
+    const a = document.createElement('a')
+    a.innerHTML = child.title
+    a.href = child.url
+    elem.appendChild(a)
+    return
+  }else{
+    child.children.forEach((child) => {
+      const h3 = document.createElement('h3')
+      h3.innerHTML = child.title
+      h3.href = child.url
+      elem.appendChild(h3)
+      return bm.appendChild(h3, child)
+    })
+  }
 }
