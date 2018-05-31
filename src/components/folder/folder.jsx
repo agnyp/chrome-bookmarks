f3bf818f05f50b487d8636b42bf5839a222218b5
Carsten Lamm <CarstenLamm@googlemail.com>
2018-05-31 20:55:39 +0200

    Implement basic folder component

--- src/components/folder/folder.jsx
+++ src/components/folder/folder.jsx
@@ -1,7 +1,33 @@
-import { Component } from "react";
+import React, { Component } from "react";
+
+const renderWhen = (cond, component) => {
+  if(cond) {
+    return component;
+  }
+
+  return null;
+}
 
 class Folder extends Component {
-  render(){}
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      isOpen: this.props.open
+    }
+  }
+
+  render(){
+    return (
+      <div className={`folder ${this.props.className}`}>
+        <h3 className='title' onClick={ (evt) => this.setState({ isOpen: !this.state.isOpen })}>{this.props.title}</h3>
+        <div className='folder-content'>
+          { renderWhen(this.state.isOpen, this.props.children) }
+        </div>
+      </div>
+    )
+  }
 }
 
 export { Folder };
+
--- src/components/folder/folder.test.jsx
+++ src/components/folder/folder.test.jsx
@@ -2,8 +2,64 @@ import React from "react";
 import { shallow } from "enzyme";
 import { Folder } from "./folder";
 
+const renderComponent = (props) => shallow(<Folder {...props} />);
+
 describe("Folder component", () => {
   test("Component exports its name", ()=>{
-    expect(shallow(<Folder></Folder>)).toExist();
+    const component = renderComponent();
+
+    expect(component).toExist();
+  });
+
+  test('component has its className', () => {
+    const component = renderComponent();
+
+    expect(component).toHaveClassName('folder');
+  });
+
+  test('additional classes are applied', () => {
+    const component = renderComponent({ className: 'foo' });
+
+    expect(component).toHaveClassName('foo');
+  });
+
+  test('renders given links', () => {
+    const component = shallow(<Folder open><a href='#'>Food</a></Folder>);
+
+    expect(component).toHaveText('Food');
+  })
+
+  test('renders given Folder', () => {
+    const dummyFolder = <Folder className='inner-folder' />
+    const component = renderComponent({ open: true, children: dummyFolder });
+
+    expect(component.find('.inner-folder')).toExist();
+  })
+
+  test('renders a title', () => {
+    const component = renderComponent({ title: 'FRS24' });
+
+    expect(component).toHaveText('FRS24');
+  })
+
+  test('a closed folder shows no content', () => {
+    const component = renderComponent({ open: false, children: "Link" });
+
+    expect(component).toHaveText('');
+  });
+
+  test('a closed folder can be opened', () => {
+    const component = renderComponent({ open: false, children: "Link" });
+
+    expect(component).toHaveText('');
+
+    component.find('.title').simulate('click');
+
+    component.update();
+
+    expect(component).toHaveText('Link');
   });
 });
+
+
+
