6afe711aa79c7c67e960f5b94358e644759b75ea
Carsten Lamm <CarstenLamm@googlemail.com>
2018-05-31 15:49:03 +0200

    Add enzyme for better react app testing
    Ref: http://airbnb.io/enzyme/
    
    Add enzyme-jest matcher
    
    Ref: https://github.com/FormidableLabs/enzyme-matchers#assertions

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import { mount } from 'enzyme';
+
 import ReactDOM from 'react-dom';
 import App from './App';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+const renderComponent = () => mount(<App />);
+
+describe('App component', () => {
+  test('renders the app', () => {
+    expect(renderComponent()).toExist()
+  });
 });
+
